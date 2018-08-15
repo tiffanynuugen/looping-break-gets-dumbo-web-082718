df66048c898385a52a802df47a40b40802e49d31
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 00:40:23 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -4,6 +4,6 @@ def levitation_quiz
     puts "What is the spell that enacts levitation?"
     answer = gets.chomp
     break if answer == "Wingardium Leviosa"
-    puts "You passed the quiz!"
   end
+  puts "You passed the quiz!"
 end
