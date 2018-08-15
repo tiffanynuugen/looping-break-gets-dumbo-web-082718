6ba28c29069d7e173edc26a9885e5f60273c0e54
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 00:39:33 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
+  loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+    puts "You passed the quiz!"
+  end
 	#your code here
 end
-
-
